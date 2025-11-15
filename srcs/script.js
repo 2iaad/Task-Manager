1a6d2bd57f8abf2928721aa2a63f1356ade6414f
Ziad Derfoufi <ziadderfoufi@Ziads-MacBook-Pro.local>
2025-11-15 14:47:46 +0100

    [Updating data so the list persists across page refreshes]

--- srcs/script.js
+++ srcs/script.js
@@ -40,6 +40,52 @@ function AddTask(event) // pass the event to remove the "?"
 
         li.appendChild(liRm) // append cross to list
         
-        inputText.value = "";
     }
+    inputText.value = "";
+    saveData();
 }
+
+/**
+ * event listener for manipulation the list elements:
+ *      -> Mark task as done.
+ *      -> Delete task.
+ */
+
+listContainer.addEventListener( "click",    // first parameter      -> (Event type)
+                                function(e) // second parameter    -> (Event handler function with event object "e")
+/**
+ *  - this function run each time a click occures on the listContainer.
+ *  - e object that contains properties (informations about the click), most important one is:
+ *        e.target -> the exact element clicked
+ */
+                                {
+                                    if (e.target.tagName === "LI") {            // if user clicked on <li><li/> item (Task)
+                                        e.target.classList.toggle("checked")    // .toggle() -> Removes the token from the list if it exists, or adds it to the list if it doesn't. Returns a boolean indicating whether the token is in the list after the operation.
+                                    }
+                                    else if (e.target.tagName === "SPAN") {     // if user clicked on <span><span/> item (cross)
+                                        e.target.parentElement.remove()         // removes the parent of the <span><span/>, which is <li><li/>
+                                    }
+                                    saveData();
+                                },
+                                false)      // third parameter     -> (useCapture = false (event bubbling))
+
+
+/**
+ *  localStorage -> browser feature that lets you store data locally (in your browser) even after refreshing or closing the page.
+ */
+                                
+function    saveData()
+{
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+
+function    updateData()
+{
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+/**
+ * update the list each time the page is refreshed
+ */
+
+updateData();
